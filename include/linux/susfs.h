b54da75674119e2a205572284258f5af0563b94b
kylieeXD <khayloaftck@gmail.com>
2026-01-27 09:24:33 +0700

    spoof: uname version to 6.6.66 GKI kernel
    is that buildable?

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -11,7 +11,6 @@
 #define SUSFS_VERSION "v2.0.0"
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
 #define SUSFS_VARIANT "NON-GKI"
-#else
 #define SUSFS_VARIANT "GKI"
 #endif
 
--- kernel/sys.c
+++ kernel/sys.c
@@ -1208,7 +1208,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "uprobestatsbpfload", 18) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		if (current_uid().val == 0 && !legacy_ebpf) {
-			strcpy(tmp.release, "5.4.299");
+			strcpy(tmp.release, "6.6.66");
 			pr_debug("fake uname: %s/%d release=%s\n",
 				 current->comm, current->pid, tmp.release);
 		}
