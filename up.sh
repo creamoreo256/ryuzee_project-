5a52b7849b048bab49f251f36b21d07be06e50fc
Oreo <33594660+creamoreo256@users.noreply.github.com>
2026-01-29 14:51:22 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -8,16 +8,19 @@ CHAT_ID="$3"
 DATE=$(date "+%d %B %Y")
 TIME=$(date "+%H:%M WIB")
 
-CAPTION="UranusKernel Build Success
+CAPTION="🚀 UranusKernel Build Success
 
-Device  : surya
-File    : ${ZIP_NAME}
-Date    : ${DATE}
-Time    : ${TIME}
-Clang   : AOSP 13289611
-CI      : GitHub Actions
-Builder : ryuzee_project 
+📱 Device   : surya
+📦 File     : ${ZIP_NAME}
 
+📅 Date     : ${DATE}
+⏰ Time     : ${TIME}
+
+🛠 Clang    : AOSP 13289611
+🤖 CI       : GitHub Actions
+👤 Builder  : ryuzee_project
+
+━━━━━━━━━━━━━━━━━━━━━━━━━
 #UranusKernel #surya #kernel"
 
 curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
